64506f3b1645763ccf958b7e2855f5b9e034a6fb
Oleksii Shevchenko <uzlabini@gmail.com>
2022-10-01 22:36:46 +0200

    011022

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,28 +1,5 @@
-// // Add imports above this line
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-// import { galleryItems } from './gallery-items';
-// // Change code below this line
-// const render = galleryItems.map(item => `<div class="gallery__item">
-// <a class:"gallery__link" href="${item.original}">
-// <img class: "gallery__image" src="${item.preview}"
-// data-source="${item.original}"
-// alt= "${item.description}"/></a></div>`).join("");
-// const galleryBox = document.querySelector('.gallery');
-// galleryBox.insertAdjacentHTML("beforeend", render);
-// galleryBox.addEventListener("click", e => {
-//     e.preventDefault();
-//     const lightbox = new SimpleLightbox(".gallery a", {
-//         captionsData: "alt",
-//         captionDelay: 250,
-//     })
-// })
-
-
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-
 
 // Change code below this line
 console.log(galleryItems);
