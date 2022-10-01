dfdef40f4bcb3268f3a49dc19e277b0fd7113110
Oleksii Shevchenko <uzlabini@gmail.com>
2022-10-01 21:16:04 +0200

    01.10.2022

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,46 @@
+// // Add imports above this line
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+// const render = galleryItems.map(item => `<div class="gallery__item">
+// <a class:"gallery__link" href="${item.original}">
+// <img class: "gallery__image" src="${item.preview}"
+// data-source="${item.original}"
+// alt= "${item.description}"/></a></div>`).join("");
+// const galleryBox = document.querySelector('.gallery');
+// galleryBox.insertAdjacentHTML("beforeend", render);
+// galleryBox.addEventListener("click", e => {
+//     e.preventDefault();
+//     const lightbox = new SimpleLightbox(".gallery a", {
+//         captionsData: "alt",
+//         captionDelay: 250,
+//     })
+// })
+
+
+
 // Add imports above this line
-import simpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+
+
 // Change code below this line
-console.log(simpleLightbox);
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryTemplate =({preview, original, description}) =>`
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+
+const imagesContainer = document.querySelector(".gallery");
+const galleryList = galleryItems.map(galleryItem => galleryTemplate(galleryItem)).join('');
+imagesContainer.insertAdjacentHTML("afterbegin", galleryList);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+	captionDelay: 250
+})
